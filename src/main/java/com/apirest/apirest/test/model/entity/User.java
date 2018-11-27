eff78d696bb6b6019f2e89e2758c362352db4545
seridan <seridan01@hotmail.com>
2018-11-27 09:07:43 +0000

    configurado para que funcione sin implementar la seguridad

--- src/main/java/com/apirest/apirest/test/model/entity/User.java
+++ src/main/java/com/apirest/apirest/test/model/entity/User.java
@@ -34,9 +34,16 @@ public class  User implements Serializable {
         this.name = name;
     }
 
-    public User(Long id, String name) {
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public User(Long id, String name, String password, List<Role> roles) {
         this.id = id;
         this.name = name;
+        this.password = password;
+        this.roles = roles;
     }
 
     public Long getId() {
@@ -52,5 +59,19 @@ public class  User implements Serializable {
         this.name = name;
     }
 
+    public String getPassword() {
+        return password;
+    }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
 }
